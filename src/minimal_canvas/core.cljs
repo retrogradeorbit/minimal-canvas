8fd0d494f31f5f74cfa784447a7ff30bffbdab30
Crispin Wellington <retrogradeorbit@gmail.com>
2016-08-18 08:25:56 +0800

    request anim frame

--- src/minimal_canvas/core.cljs
+++ src/minimal_canvas/core.cljs
@@ -4,6 +4,31 @@
   (:require-macros
    [cljs.core.async.macros :refer [go]]))
 
+(defn make-request-animation-frame
+  "compose a function that is the r-a-f func. returns a function. This returned function takes a callback and ensures
+  its called next frame"
+  []
+  (cond
+   (.-requestAnimationFrame js/window)
+   #(.requestAnimationFrame js/window %)
+
+   (.-webkitRequestAnimationFrame js/window)
+   #(.webkitRequestAnimationFrame js/window %)
+
+   (.-mozRequestAnimationFrame js/window)
+   #(.mozRequestAnimationFrame js/window %)
+
+   (.-oRequestAnimationFrame js/window)
+   #(.oRequestAnimationFrame js/window %)
+
+   (.-msRequestAnimationFrame js/window)
+   #(.msRequestAnimationFrame js/window %)
+
+   :else
+   #(.setTimeout js/window % (/ 1000 60))))
+
+;; build the actual function
+(def request-animation-frame (make-request-animation-frame))
 (let [canvas (js/document.getElementById "my-canvas")
       ctx (.getContext canvas "2d")]
   (set! (.-fillStyle ctx) "green")
